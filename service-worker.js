11f82d520fccb9aad5f09651fdcf29bce336cf85
chocomint024 <160242179+chocomint024@users.noreply.github.com>
2025-04-10 00:27:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,38 @@
+const CACHE_NAME = 'denpa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/images/monsho_192x192.png',
+  '/images/monsho_512x512.png',
+];
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/icon-192x192.png',
-                '/icon-512x512.png',
-            ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
